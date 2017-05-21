a46fc560d23b19e20a3405cf8159b8b616861fdf
Cole <cole.trammer@gmail.com>
2017-05-21 17:55:23 +0000

    Finished the service

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-const express = require("express")
-const app = express()
+"use strict";
+const express = require("express");
+const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+const months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
 
-app.listen(process.env.PORT)
+app.get("/:date", (req, res) => {
+    let date = null;
+    const input = req.params.date;
+    if (input.match(/\d/g).length == input.length) {
+        const milliseconds = parseInt(input, 10) * 1000;
+        date = new Date(milliseconds);
+    }
+    if (date === null) {
+        date = new Date(input);
+    }
+    res.send({
+        unix: date.getTime() / 1000,
+        natural: getReadableDate(date)
+    });
+});
+
+app.listen(process.env.PORT);
+
+function getReadableDate(date) {
+    if (date.toString() === "Invalid Date") {
+        return null;
+    } else {
+        return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+    }
+}
